56a5183306d630cee674a821c038361f426310e6
RCS111 <jaycyme@gmail.com>
2021-08-17 21:04:28 +0800

    adjusted dashboard

--- src/sections/dashboardSection/components/DashboardProjectCard.js
+++ src/sections/dashboardSection/components/DashboardProjectCard.js
@@ -1,19 +1,54 @@
-import { Card, Typography, makeStyles } from "@material-ui/core";
+import {
+  Card,
+  Typography,
+  makeStyles,
+  Grid,
+  Container,
+} from "@material-ui/core";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import React from "react";
 
-const useStyles = makeStyles({
-  crd: {
-      marginTop: 25
-  }
-})
+function DashboardProjectCard({ title, count, color }) {
+  const useStyles = makeStyles({
+    crd: {
+      marginTop: 10,
+      marginBottom: 10,
+      border: 1,
+      borderStyle: "Solid",
+      outlineColor: { color },
+      borderRadius: "20px 20px 20px 20px"
+    },
+    dot: {
+      color,
+      textAlign: "Right",
+    },
+    padTop: {
+      marginTop: 10
+    },
+    padBot: {
+      marginBottom: 5
+    },
 
-function DashboardProjectCard({ title, count }) {
-  const classes = useStyles()
+  });
+
+  const classes = useStyles();
 
   return (
     <Card className={classes.crd} variant="outlined">
-      <Typography variant="h6">{title}</Typography>
-      <Typography variant="h3">{count}</Typography>
+      <Container>
+        <Grid className={classes.padTop} container>
+          <Grid item xs={11}>
+            <Typography variant="h5">{title}</Typography>
+          </Grid>
+          <Grid item xs={1}>
+            <FiberManualRecordIcon className={classes.dot} />
+          </Grid>
+        </Grid>
+
+        <Grid className={classes.padBot} item xs={12}>
+          <Typography variant="h3">{count}</Typography>
+        </Grid>
+      </Container>
     </Card>
   );
 }
